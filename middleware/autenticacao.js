257fcd1ee362f6fd247099305551bca02e633092
VitorHBR <vitorhugo1923@hotmail.com>
2024-05-01 17:09:20 -0300

    controle nivel de acesso

--- middleware/autenticacao.js
+++ middleware/autenticacao.js
@@ -5,8 +5,19 @@ class Autenticacao {
     }
 
     usuarioEstaLogado(req, res, next) {
+        console.log(req.originalUrl);
         if(req.headers.cookie != undefined && req.headers.cookie.includes('usuarioLogado')) {
-            next();
+
+            if (req.headers.cookie.includes('administrador')) {
+                next();
+            } else {
+                if(req.originalUrl.includes('funcionarios')||req.originalUrl.includes('cargos')||req.originalUrl.includes('departamentos')||req.originalUrl.includes('escalas')||req.originalUrl.includes('registropontoadmin')||req.originalUrl.includes('aprovarferias'))
+                res.status(403).send('Acesso negado. Você não tem permissão para acessar esta página.');
+
+                else
+                next();
+            }
+            
         }
         else{
             res.redirect('/login');
